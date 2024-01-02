ead14f1a7fbceb96d78a0defdd1d940d2b667a05
Tanmoy Biswas <tanmoyb.cse@gmail.com>
2024-01-03 02:51:52 +0600

    Layout building

--- resources/views/pages-starter.blade.php
+++ resources/views/pages-starter.blade.php
@@ -1,53 +1,50 @@
-@include('include.header')
+@extends('layout.master')
+        <!-- ============================================================== -->
+        <!-- Start Page Content here -->
+        <!-- ============================================================== -->
+        @section('content')
+        <div class="content-page">
+            <div class="content">
 
+                <!-- Start Content-->
+                <div class="container-fluid">
 
-            <!-- ============================================================== -->
-            <!-- Start Page Content here -->
-            <!-- ============================================================== -->
-
-            <div class="content-page">
-                <div class="content">
-
-                    <!-- Start Content-->
-                    <div class="container-fluid">
-
-                        <!-- start page title -->
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="page-title-box">
-                                    <div class="page-title-right">
-                                        <ol class="breadcrumb m-0">
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Velonic</a></li>
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Pages</a></li>
-                                            <li class="breadcrumb-item active">Starter</li>
-                                        </ol>
-                                    </div>
-                                    <h4 class="page-title">Starter</h4>
+                    <!-- start page title -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title-box">
+                                <div class="page-title-right">
+                                    <ol class="breadcrumb m-0">
+                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Velonic</a></li>
+                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Pages</a></li>
+                                        <li class="breadcrumb-item active">Starter</li>
+                                    </ol>
                                 </div>
-                            </div>
-                        </div>
-                        <!-- end page title -->
-
-                    </div> <!-- container -->
-
-                </div> <!-- content -->
-
-                <!-- Footer Start -->
-                <footer class="footer">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-12 text-center">
-                                <script>document.write(new Date().getFullYear())</script> © Velonic - Theme by <b>Techzaa</b>
+                                <h4 class="page-title">Starter</h4>
                             </div>
                         </div>
                     </div>
-                </footer>
-                <!-- end Footer -->
+                    <!-- end page title -->
 
-            </div>
+                </div> <!-- container -->
 
-            <!-- ============================================================== -->
-            <!-- End Page content -->
-            <!-- ============================================================== -->
+            </div> <!-- content -->
 
-@include('include.footer')
+            <!-- Footer Start -->
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12 text-center">
+                            <script>document.write(new Date().getFullYear())</script> © Velonic - Theme by <b>Techzaa</b>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <!-- end Footer -->
+
+        </div>
+        @endsection
+        <!-- ============================================================== -->
+        <!-- End Page content -->
+        <!-- ============================================================== -->
+
